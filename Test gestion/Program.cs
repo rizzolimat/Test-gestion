32c84900083dae4cd19aa243e75f6b117c4497d7
rizzolimat <rizzolimatias@gmail.com>
2022-05-16 19:07:24 -0300

    Se agrega el Proyecto

---
+++ Test gestion/Program.cs
@@ -0,0 +1,10 @@
+﻿int id = 1;
+string tittle = "Playstation 4";
+int category_id = 4;
+string category_name = "Entretenimiento";
+
+Console.WriteLine("ID: "+id);
+Console.WriteLine("TITULO: " + tittle);
+Console.WriteLine("ID Categoria: " + category_id);
+Console.WriteLine("Nombre de categoria: " + category_name);      
+Console.ReadKey();
